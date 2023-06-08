56d62331aef954393d41d226e1953012075a4dff
AiBryce <bryceai@foxmail.com>
2023-06-08 22:33:25 +0800

    V 1.1.3 修复检测文件不显示信息问题

--- widget.cpp
+++ widget.cpp
@@ -22,9 +22,16 @@ Widget::Widget(QWidget *parent)
     ui->progressBar->setRange(0,100);
     //当lineEdit的文本发生改变，在textEdit中打印文本更改内容
     connect(this->ui->lineEdit,&QLineEdit::textChanged,this,[=](){
+
        this->ui->textEdit->append(QString("已经检测到："));
-       this->ui->textEdit->append(ui->lineEdit_2->text());
+       this->ui->textEdit->append(ui->lineEdit->text());
     });
+
+    //设置提示信息
+    ui->lineEdit->setPlaceholderText("点击 选择文件 按钮选择文件或输入文件的绝对路径。");
+    ui->lineEdit_2->setPlaceholderText("点击 生成密钥 按钮进行RSA混合加密或直接输入字符（支持中文）。");
+    ui->textEdit->setPlaceholderText("信息安全大作业\n小组成员：艾庆陆，黄伟，高浩天，徐玉琴。\n20230608 V1.1.2\n--------------------------------\n点击 选择文件 按钮选择文件或输入文件的绝对路径。\n点击 生成密钥 按钮进行RSA混合加密或直接输入字符（支持中文）。\n--------------------------------");
+
 }
 
 Widget::~Widget()
